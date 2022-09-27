d52fd29d1161e477cfa150ac309ceeadbbaa7db8
Panucio31 <m.januska1990@gmail.com>
2022-09-27 17:50:34 +0300

    feat: :sparkles: JS validates password input

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-const passwordField = document.querySelector('#password');
-const passwordConfirm = document.querySelector('#confirm-password');
+const submitBtn = document.getElementById('submit-btn')
 
+function checkPassword(event) {
+  event.preventDefault();
+
+  const passwordField = document.querySelector("#password").value;
+  const passwordConfirm = document.querySelector("#confirm-password").value;
+  if (passwordField === '' || passwordConfirm === '') {
+    alert('Please enter Password')
+  }
+  if (passwordField !== passwordConfirm) {
+    alert('Passwords did not match')
+  }
+  
+  
+  
+}
+
+submitBtn.addEventListener('click', checkPassword)
 
