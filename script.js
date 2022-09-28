3319203b1365468f9fa5f3d00ac2793e35d2e28d
Panucio31 <m.januska1990@gmail.com>
2022-09-28 19:48:33 +0300

    fix: :bug: improved js validation

--- script.js
+++ script.js
@@ -1,36 +1,26 @@
 const submitBtn = document.getElementById("submit-btn");
 const passwordField = document.querySelector("#password");
 const passwordConfirm = document.querySelector("#confirm-password");
-const passwordInputContainer = document.querySelector("#inputP");
-const checkPasswordContainer = document.querySelector("#inputCp");
-const warningText = document.createElement("h6");
+const passwordErrorText = document.getElementById("password-error");
 
-const wrongPassword = () => {
-  passwordField.style.outline = "2px solid red";
-  passwordConfirm.style.outline = "2px solid red";
+const highlightPasswordElements = () => {
+  passwordField.style.borderColor = "red";
+  passwordConfirm.style.borderColor = "red";
+
+  passwordErrorText.classList.remove("hide");
 };
 
 function checkPassword(event) {
-  if (passwordField.value === "" || passwordConfirm.value === "") {
-    warningText.innerHTML = "*Please enter password";
-    warningText.style.color = "red";
-    passwordInputContainer.append(warningText);
-    wrongPassword();
+  if (!passwordField.value || !passwordConfirm.value) {
+    return undefined;
+  }
+  if (passwordField.value !== passwordConfirm.value) {
+    highlightPasswordElements();
     event.preventDefault();
     return undefined;
   }
-  if 
-    (passwordField.value !== passwordConfirm.value && (passwordField.value !== "" || passwordConfirm.value !== "")) {
-    warningText.innerHTML = "*Passwords do not Match";
-    warningText.style.color = "red";
-    checkPasswordContainer.append(warningText);
-    event.preventDefault();
-    return undefined;
-  }
-  if (passwordField.value === passwordConfirm.value) {
-    return;
-  }
-
+ alert('Huray!!!');
+ return undefined;
 }
 
 submitBtn.addEventListener("click", checkPassword);
